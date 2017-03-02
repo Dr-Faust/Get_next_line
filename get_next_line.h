54e682606860d7e64641664e8bfd865ace701e75
Oleksandr Podolian <opodolia@e1r8p1.unit.ua>
2017-03-02 18:32:11 +0200

    get_next_line

---
+++ get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: opodolia <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/17 17:28:00 by opodolia          #+#    #+#             */
+/*   Updated: 2017/01/25 14:52:01 by opodolia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 32
+# include "libft/libft.h"
+
+typedef	struct		s_get
+{
+	void			*content;
+	size_t			content_size;
+	struct s_get	*next;
+	int				fd;
+}					t_get;
+
+int					get_next_line(const int fd, char **line);
+
+#endif
